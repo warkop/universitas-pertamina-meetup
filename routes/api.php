22aec8160edb50acb309b7fbf5f784c5fafa08d9
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-08-26 17:23:18 +0700

    prepare route

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix' => 'title'], function () {
+    Route::get('/', 'TitleController@index');
+    Route::get('/{title}', 'TitleController@show');
+    Route::post('/', 'TitleController@store');
+    Route::put('/{title}', 'TitleController@store');
+    Route::delete('/{title}', 'TitleController@destroy');
+});
+
+Route::group('nationality')->get('/', function (Request $request) {
+    Route::get('/', 'NationalityController@index');
+    Route::get('/{nationality}', 'NationalityController@show');
+    Route::post('/', 'NationalityController@store');
+    Route::put('/{nationality}', 'NationalityController@store');
+    Route::delete('/{nationality}', 'NationalityController@destroy');
+});
