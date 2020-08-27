b26920ef7dcb7951a14b6a38f5488b808cbe62b1
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-08-27 15:45:59 +0700

    add model

---
+++ app/Model/Member.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+
+class Member extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'member';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $hidden = [
+        'deleted_at',
+        'deleted_by',
+    ];
+
+    public static function listData($start, $length, $search = '', $count = false, $sort, $field, $options = [])
+    {
+        $result = DB::table('institution');
+
+        if (!empty($search)) {
+            $result = $result->where(function ($where) use ($search) {
+                $where->where('name', 'ILIKE', '%' . $search . '%');
+            });
+        }
+
+        if ($count == true) {
+            $result = $result->count();
+        } else {
+            $result  = $result->offset($start)->limit($length)->orderBy($field, $sort)->get();
+        }
+
+        return $result;
+    }
+}
