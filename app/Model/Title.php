06ee1eba406a41a3cc6b4d9279f473236156fa99
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-08-27 08:38:03 +0700

    title modul

---
+++ app/Model/Title.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+
+class Title extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'title';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $hidden = [
+        'deleted_at',
+        'deleted_by',
+    ];
+
+    protected $dates = ['created_at', 'updated_at'];
+
+    public static function listData($start, $length, $search = '', $count = false, $sort, $field, $options = [])
+    {
+        $result = DB::table('title');
+
+        if (!empty($search)) {
+            $result = $result->where(function ($where) use ($search) {
+                $where->where('name', 'ILIKE', '%' . $search . '%');
+            });
+        }
+
+        if ($count == true) {
+            $result = $result->count();
+        } else {
+            $result  = $result->offset($start)->limit($length)->orderBy($field, $sort)->get();
+        }
+
+        return $result;
+    }
+}
