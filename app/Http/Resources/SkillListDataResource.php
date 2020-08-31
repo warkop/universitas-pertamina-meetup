8077e9b297679d9c0cd01fb66ba8bafce32610da
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-08-31 14:21:15 +0700

    update resource SkillListDataResource

--- app/Http/Resources/SkillListDataResource.php
+++ app/Http/Resources/SkillListDataResource.php
@@ -14,6 +14,11 @@ class SkillListDataResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id'            => $this->id,
+            'name'          => $this->name,
+            'type'          => $this->type,
+            'updated_at'    => date('d-m-Y', strtotime($this->updated_at)),
+        ];
     }
 }
