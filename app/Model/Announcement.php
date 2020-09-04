85ecfe48ee4f3fd1ad894b436e6d7801e13453d7
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-09-04 22:35:48 +0700

    modul announcement

---
+++ app/Model/Announcement.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+use Wildside\Userstamps\Userstamps;
+
+class Announcement extends Model
+{
+    use SoftDeletes;
+    use Userstamps;
+
+    protected $table = 'announcement';
+    protected $primaryKey = 'id';
+
+    protected $guarded = [
+        'id'
+    ];
+
+    protected $hidden = [
+        'created_by',
+        'updated_by',
+        'deleted_at',
+        'deleted_by',
+    ];
+
+    protected $with = [
+        'comment.userComment'
+    ];
+
+    public function comment()
+    {
+        return $this->hasMany(AnnouncementComment::class);
+    }
+
+    public static function listData($start, $length, $search = '', $count = false, $sort, $field, $options = [])
+    {
+        $result = DB::table('announcement')->whereNull('announcement.deleted_at');
+
+        if (!empty($search)) {
+            $result = $result->where(function ($where) use ($search) {
+                $where->where('name', 'ILIKE', '%' . $search . '%');
+            });
+        }
+
+        if ($count == true) {
+            $result = $result->count();
+        } else {
+            $result  = $result->offset($start)->limit($length)->orderBy($field, $sort)->get();
+        }
+
+        return $result;
+    }
+}
