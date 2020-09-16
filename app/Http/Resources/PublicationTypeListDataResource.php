7f37141d9bf6c9eff97c675c4fa13803c65e0756
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-09-16 15:52:09 +0700

    update collection

--- app/Http/Resources/PublicationTypeListDataResource.php
+++ app/Http/Resources/PublicationTypeListDataResource.php
@@ -17,8 +17,8 @@ class PublicationTypeListDataResource extends JsonResource
         return [
             'id'            => $this->id,
             'name'          => $this->name,
-            'created_at'    => $this->created_at,
-            'updated_at'    => $this->updated_at,
+            'created_at'    => date('d-m-Y H:i:s', strtotime($this->created_at)),
+            'updated_at'    => date('d-m-Y H:i:s', strtotime($this->updated_at)),
         ];
     }
 }
