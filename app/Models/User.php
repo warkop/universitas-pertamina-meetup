6b0435e1d0c6178828bce0efdd970f05f1f7ede2
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-09-18 00:39:41 +0700

    add more column for fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,7 +19,7 @@ class User extends Authenticatable implements JWTSubject, MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'email', 'password', 'type', 'role_id', 'owner_id'
     ];
 
     /**
