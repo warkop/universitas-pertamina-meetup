7b847febb9ef0035c0f5be8fd55430c7f47d0325
Fahmi Rizal <fahmirizal96@gmail.com>
2020-09-29 10:49:48 +0700

    change

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -65,7 +65,7 @@ class AuthController extends Controller
 
        $data  = $data_by_role->union($data_by_user)->groupBy('menu.id', 'role_menu.action')->orderBy('order', 'asc')->get();
 
-       $change_mail = ($user->new_email != null)? TRUE : FALSE,
+       $change_mail = ($user->new_email != null)? TRUE : FALSE;
 
        return $this->respondWithToken($token, $change_mail, SidebarMenuDataResource::collection($data));
     }
