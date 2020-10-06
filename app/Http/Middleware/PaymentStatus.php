eba8b668704f1a958834b3d4bb231fa276a07ee4
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-10-06 16:05:04 +0700

    payment status middleware

--- app/Http/Middleware/PaymentStatus.php
+++ app/Http/Middleware/PaymentStatus.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use App\Helpers\HelperPublic;
 use App\Models\Invoice;
+use App\Models\PaymentToken;
 use Closure;
 use Illuminate\Http\Request;
 
@@ -23,10 +24,17 @@ class PaymentStatus
             $invoice = new Invoice;
             $lastInvoice = $invoice->getLastInvoice($user);
             if ($lastInvoice != null && $lastInvoice->valid_until == null) {
+                $paymentToken = PaymentToken::where('invoice_id', $lastInvoice->id)->firstOrFail();
                 $data = [
                     'status' => [
                         'code' => 402,
                         'message' => 'User harus melakukan pembayaran terlebih dahulu!',
+                    ],
+                    '_link' => [
+                        'method_upload' => 'POST',
+                        'url_upload' => url('api/register/upload-payment?token='.$paymentToken->token),
+                        'method_data' => 'POST',
+                        'url_data' => url('api/register/send-data-payment?token='.$paymentToken->token),
                     ]
                 ];
                 return response()->json($data, 200);
