42cedbff7734d63d2e7423eec54cf50bd8cd7b20
Fahmi Rizal <fahmirizal96@gmail.com>
2020-10-07 12:15:02 +0700

    new email

--- app/Services/MailService.php
+++ app/Services/MailService.php
@@ -5,6 +5,8 @@ use App\Mail\VerifyChangeMail;
 use App\Mail\ResetPassword;
 use App\Mail\Approved;
 use App\Mail\Disapproved;
+use App\Mail\ApprovedPayment;
+use App\Mail\DeclinePayment;
 use App\Mail\Invoice as MailInvoice;
 use App\Mail\VerifyMail;
 use App\Models\EmailReset;
@@ -13,6 +15,7 @@ use App\Models\Invoice;
 use App\Models\Member;
 use App\Models\Package;
 use App\Models\User;
+use App\Helpers\HelperPublic;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Str;
 
@@ -96,15 +99,60 @@ class MailService
 
       $package = Package::find($invoice->package_id);
 
+      $helper = new HelperPublic;
+
       $dataMail = [
          'name' => $model->name,
          'email' => $model->email,
          'orderDate' => date("F j, Y", strtotime($invoice->created_at)),
          'packageName' => $package->name,
-         'price' => $invoice->price,
+         'price' => $helper->helpCurrency($invoice->price),
          'url' => env('URL_FRONTEND').'/renew-package',
       ];
 
       Mail::to($user->email)->send(new MailInvoice($dataMail));
+   }
+
+   public function sendDeclinePayment(Invoice $invoice): void
+   {
+      $user = User::find($invoice->user_id);
+
+      if ($user->type == 0) {
+         $model = Institution::find($user->owner_id);
+      } else if ($user->type == 1) {
+         $model = Member::find($user->owner_id);
+      }
+
+      $dataMail = [
+         'email' => $model->email,
+      ];
+
+      Mail::to($user->email)->send(new DeclinePayment($dataMail));
+   }
+
+   public function sendApprovedPayment(Invoice $invoice): void
+   {
+      $user = User::find($invoice->user_id);
+
+      if ($user->type == 0) {
+         $model = Institution::find($user->owner_id);
+      } else if ($user->type == 1) {
+         $model = Member::find($user->owner_id);
+      }
+
+      $package = Package::find($invoice->package_id);
+
+      $helper = new HelperPublic;
+
+      $dataMail = [
+         'name' => $model->name,
+         'email' => $model->email,
+         'number' => $invoice->number,
+         'packageName' => $package->name,
+         'price' => $helper->helpCurrency($invoice->price),
+         'url' => env('URL_FRONTEND').'/renew-package',
+      ];
+
+      Mail::to($user->email)->send(new ApprovedPayment($dataMail));
    }
 }
