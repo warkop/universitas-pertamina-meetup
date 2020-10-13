db7f532ad1df2572f0d8b526921265f880ec57d4
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-10-13 13:57:09 +0700

    job renew invoice

---
+++ app/Jobs/RenewInvoice.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Invoice;
+use App\Models\User;
+use App\Services\PaymentService;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class RenewInvoice implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $user;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+
+        $paymentService = new PaymentService;
+        $paymentService->sendNearExpirated($this->user);
+    }
+}
