c86fec616e51b2ed79d422df1248b55320ebcaeb
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-10-17 14:27:01 +0700

    complete registration with conditional extension or independent

--- app/Services/RegisterService.php
+++ app/Services/RegisterService.php
@@ -1,14 +1,53 @@
 <?php
 namespace App\Services;
 
+use App\Models\Department;
 use App\Models\Institution;
+use App\Models\Invoice;
+use App\Models\Member;
+use App\Models\Package;
 use App\Models\User;
 use Exception;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Hash;
 
 class RegisterService
 {
+    private function createUser($request, array $spesific)
+    {
+        $user = User::create([
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password),
+            'type'      => $spesific['type'],
+            'role_id'   => $spesific['role_id'],
+            'owner_id'  => $spesific['id'],
+        ]);
+
+        return $user;
+    }
+
+    private function isQuotaAvailable(Department $department, $package_id)
+    {
+        $countMember = Member::whereHas('department', function($query) use($department){
+            $query->where('institution_id', $department->institution_id);
+        })->count();
+
+        $package = Package::find($package_id);
+
+        if ($countMember < $package->max_member) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private function getInstitutionPackage(Department $department)
+    {
+        $institution = Institution::findOrFail($department->institution_id);
+        $user = User::where('owner_id', $institution->id)->firstOrFail();
+        $invoice = (new Invoice)->getLastPaidedInvoice($user);
+        return $invoice->package_id;
+    }
+
     public function registerInstitution($request)
     {
         $package_id = $request->input('package_id');
@@ -21,28 +60,76 @@ class RegisterService
                 'role_id'   => 3,
                 'type'      => 0,
             ];
-            $user = User::create([
-                'email'     => $request->email,
-                'password'  => Hash::make($request->password),
-                'type'      => $spesific['type'],
-                'role_id'   => $spesific['role_id'],
-                'owner_id'  => $spesific['id'],
-            ]);
+
+            $user = $this->createUser($request, $spesific);
             DB::commit();
         } catch (Exception $ex) {
             DB::rollback();
             return response()->json(['error' => $ex->getMessage()], 500);
         }
 
-        $paymentService = new PaymentService;
         return [
-            'token' => $paymentService->registerPackage($package_id, $user),
+            'token' => (new PaymentService)->registerPackage($package_id, $user),
             'model' => $institution
         ];
     }
 
     public function registerResearcher($request)
     {
+        DB::beginTransaction();
+        try {
+            $member = new Member();
 
+            if ($request->department_id) {
+                $department = Department::findOrFail($request->department_id);
+                $package_id = $this->getInstitutionPackage($department);
+                $isQuotaAvailable = $this->isQuotaAvailable($department, $package_id);
+                if (!$isQuotaAvailable) {
+                    throw new Exception('Kuota institusi sudah penuh, silahkan Anda mendaftar sebagai independent atau extension', 403);
+                }
+                $member->department_id      = $request->department_id;
+            } else {
+                $member->is_independent = true;
+                $package_id = $request->input('package_id');
+            }
+
+            $member->name               = $request->name;
+            $member->title_id           = $request->title_id;
+            $member->nationality_id     = $request->nationality_id;
+            $member->employee_number    = $request->employee_number;
+            $member->office_address     = $request->office_address;
+            $member->office_phone_number= $request->office_phone_number;
+            $member->email              = $request->email;
+            $member->save();
+
+            $spesific = [
+                'id'        => $member->id,
+                'role_id'   => 2,
+                'type'      => 1,
+            ];
+
+            $user = $this->createUser($request, $spesific);
+
+            // register package
+            $paymentToken = null;
+            if ($request->is_extenstion || $member->is_independent) {
+                $paymentToken = (new PaymentService)->registerPackage($package_id, $user);
+            }
+            DB::commit();
+        } catch (Exception $ex) {
+            DB::rollback();
+            return response()->json([
+                'status' => [
+                    'code' => $ex->getCode(),
+                    'message' => $ex->getMessage()
+                ],
+                'data' => null
+            ], $ex->getCode());
+        }
+
+        return [
+            'token' => $paymentToken,
+            'model' => $member
+        ];
     }
 }
