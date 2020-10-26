8e1e1cb718b5af137c2e492fa63bbc82bd1bf79a
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-10-26 07:49:26 +0700

    for testing datatable

--- resources/views/datatable.blade.php
+++ resources/views/datatable.blade.php
@@ -17,22 +17,24 @@
 <script src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js"></script>
 <script type="text/javascript">
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoIiwiaWF0IjoxNjAzNDQxMDk3LCJleHAiOjE2MDM1Mjc0OTcsIm5iZiI6MTYwMzQ0MTA5NywianRpIjoienowc0ZhOHBSdjAyRU11byIsInN1YiI6MSwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.n05i2mD_2mqiC_FV2CbfrT_JnpkCRM9Ew_ElTaCGWlQ";
     $(document).ready(function() {
         $('#example').DataTable( {
             "processing": true,
             "serverSide": true,
+
             "ajax": {
-                "url": "api/opportunity",
+                "url": "api/payment?type=1",
                 "beforeSend": function (xhr) {
                     xhr.setRequestHeader('Authorization',
-                        "Bearer " + "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC8xMjcuMC4wLjE6ODAwMFwvYXBpXC9hdXRoIiwiaWF0IjoxNjAyODI5MjE2LCJleHAiOjE2MDI5MTU2MTYsIm5iZiI6MTYwMjgyOTIxNiwianRpIjoieFlvQ0pNanhmY3hpMVl6UiIsInN1YiI6MSwicHJ2IjoiMjNiZDVjODk0OWY2MDBhZGIzOWU3MDFjNDAwODcyZGI3YTU5NzZmNyJ9.wzkEPlZipkmmtIkRLgx4yss_KXbZH42BgM6GJQ9zMu4"
+                        "Bearer " +token
                     )
                 }
             },
             "columns": [
                 {data: "name"},
                 {data: "institution_name"},
-                {data: "opportunity_type_name"}
+                {data: "department_name"}
             ]
         } );
     } );
