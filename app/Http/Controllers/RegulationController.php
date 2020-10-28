8dc16a9f1b65580ae9ca4150ce446cd084c0b63c
Mohammad Khafid Barokum <navigator619@gmail.com>
2020-10-28 18:35:08 +0700

    institution regulation

--- app/Http/Controllers/RegulationController.php
+++ app/Http/Controllers/RegulationController.php
@@ -170,7 +170,7 @@ class RegulationController extends Controller
     public function show(Regulation $regulation)
     {
         $this->responseCode = 200;
-        $this->responseData = $regulation;
+        $this->responseData = $regulation->load('institutionRegulation');
 
         return response()->json($this->getResponse(), $this->responseCode);
     }
